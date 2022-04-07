f63525026553fade8328a1072edbb1bc4c532554
lirons1992 <103179526+lirons1992@users.noreply.github.com>
2022-04-07 11:55:28 +0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,9 @@
+#All variables will be in this file
+
+variable "access_key" {
+default = "AKIAXA6RT4K7WJGH6IVO"
+}
+variable "secret_key" {
+default = "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"
+}
+
